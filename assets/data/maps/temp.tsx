0074f71d02a4335ff1aa00d492dee0e7a5f98908
Tim I Hely <seifertim@gmail.com>
2022-11-09 19:08:12 -0600

    first commit

---
+++ assets/data/maps/temp.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.1" name="temp" tilewidth="24" tileheight="24" tilecount="195" columns="15">
+ <image source="../../images/temp_tiles.png" width="360" height="312"/>
+</tileset>
---
+++ source/import.hx
@@ -0,0 +1,10 @@
+#if !macro
+import flixel.FlxG;
+import flixel.FlxGame;
+import flixel.FlxObject;
+import flixel.FlxSprite;
+import flixel.FlxState;
+import flixel.FlxSubState;
+import flixel.system.FlxAssets.FlxShader;
+import globals.Screen;
+#end
